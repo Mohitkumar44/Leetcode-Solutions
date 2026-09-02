c3f75e97477e2ae6411b64d024b6fdca582ae0d4
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-09-03 01:10:47 +0530

    Update 4Sum solution

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        if(nums.size() < 4) return {};
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i+1; j < nums.size(); j++) {
+                for(int k = j+1; k < nums.size(); k++) {
+                    for(int l = k+1; l < nums.size(); l++) {
+
+                    }
+                }
+            }
+        }
+        return {{}};
+    }
+};
