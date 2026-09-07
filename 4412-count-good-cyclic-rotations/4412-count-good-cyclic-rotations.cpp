4d38763ebb995656a277e6272341ed3abc85a423
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-09-07 16:13:10 +0530

    Update Count Good Cyclic Rotations solution

--- 4412-count-good-cyclic-rotations/4412-count-good-cyclic-rotations.cpp
+++ 4412-count-good-cyclic-rotations/4412-count-good-cyclic-rotations.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    vector<int> rotate(vector<int> s) {
-        vector<int> temp;
-        for(int i = 0; i < s.size(); i++) {
-            temp.push_back(s[i]);
-        }
-        temp[s.size()-1] = s[s.size()-1];
-        return temp;
-    }
     int countGoodRotations(vector<int>& nums) {
         int n = nums.size();
+        int l = 0, r = 0;
+        for(int i = 0; i < n/2; i++) {
+            l += nums[i];
+        }
+        for(int i = n/2; i < n; i++) {
+            r += nums[i];
+        }
+        int i = 0, j = n/2;
         int cnt = 0;
-        for(int i = 0; i < n; i++) {
-            vector<int> t = rotate(nums);
-            int score = 0;
-            nums = t;
-            for(int i = 0; i < n - 1; i++){
-                if(t[i] == t[i + 1]) score++;
-            }
-            // if(score == k) cnt++;
+        for(int k = 0; k < n; k++) {
+            if(l == r) cnt++;
+            l -= nums[i];
+            r += nums[i];
+            l += nums[j];
+            r -= nums[j];
         }
         return cnt;
     }
