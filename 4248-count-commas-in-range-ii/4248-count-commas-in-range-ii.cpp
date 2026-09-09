95012fecc4350fb284d7f9fc9bba78d6efa8910f
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-09-09 23:14:00 +0530

    Update Count Commas in Range II solution

--- 4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.cpp
@@ -2,24 +2,32 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long cnt = 0;
-        if(n >= 1000000000000000) {
-            cnt += 5*(n-999999999999999);
-            n = 999999999999999;
+        if( (long long)n >= (long long)1000000000000000) {
+            cnt+= (long long)((long long)n-(long long)9999999999999);
+            n = (long long)9999999999999;
         }
-        if(n >= 1000000000000) {
-            cnt += 4*(n-999999999999);
-            n = 999999999999;
+        if( (long long)n >= (long long)10000000000000) {
+            cnt += (long long)((long long)n-(long long)99999999999);
+            n = (long long)99999999999;
         }
-        if(n >= 1000000000) {
-            cnt += 3*(n-999999999);
-            n = 999999999;
+        if( (long long)n >= (long long)100000000000) {
+            cnt += (long long)((long long)n-(long long)999999999);
+            n = (long long)999999999;
         }
-        if(n >= 1000000) {
-            cnt += 2*(n-999999);
-            n = 999999;
+        if( (long long)n > (long long)1000000000) {
+            cnt += (long long)((long long)4*(long long)((long long)n-(long long)9999999));
+            n = (long long)9999999;
         }
-        if(n >= 1000) {
-            cnt += 2*(n-999);
+        if( (long long)n > (long long)10000000) {
+            cnt += (long long)((long long)2*(long long)((long long)n-(long long)99999));
+            n = (long long)100000;
+        }
+        if( (long long)n > (long long)100000) {
+            cnt += (long long)((long long)n-(long long)999);
+            n = (long long)999;
+        }
+        if( (long long)n > (long long)1000) {
+            cnt += (long long)((long long)n-(long long)999);
         }
         return cnt;
     }
