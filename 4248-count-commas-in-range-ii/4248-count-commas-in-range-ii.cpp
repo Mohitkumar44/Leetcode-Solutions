3bb63ab2b68cc1a89b7ef4edc1c283e00d97213f
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-09-09 18:15:39 +0530

    Update Count Commas in Range II solution

--- 4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.cpp
@@ -7,6 +7,7 @@ public:
         else if( n < (long long)1000000000) return n-9999999;
         else if( n < (long long)100000000000) return n-999999999;
         else if( n < (long long)10000000000000) return n-99999999999;
+        else if( n < (long long)1000000000000000) return n-9999999999999;
         return -1;
     }
 };
