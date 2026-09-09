5f079efe03ee11f3e22f22070908f81e076ec92a
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-09-09 22:35:17 +0530

    Update Count Commas in Range II solution

--- 4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long cnt = 0;
-        // if( (long long)n >= (long long)10000000000000000000) {
-        //     cnt += (long long)((long long)n-(long long)999999999999999); 
-        //     n = (long long)99999999999999999;
-        // } 
-        if( (long long)n >= (long long)100000000000000000) {
-            cnt += (long long)((long long)n-(long long)999999999999999);
-            n = (long long)999999999999999;
-        } 
         if( (long long)n >= (long long)1000000000000000) {
             cnt+= (long long)((long long)n-(long long)9999999999999);
             n = (long long)9999999999999;
@@ -22,19 +14,19 @@ public:
             cnt += (long long)((long long)n-(long long)999999999);
             n = (long long)999999999;
         }
-        if( (long long)n >= (long long)1000000000) {
-            cnt += (long long)((long long)n-(long long)9999999);
+        if( (long long)n > (long long)1000000000) {
+            cnt += (long long)((long long)4*(long long)((long long)n-(long long)9999999));
             n = (long long)9999999;
         }
-        if( (long long)n >= (long long)10000000) {
-            cnt += (long long)((long long)n-(long long)99999);
-            n = (long long)99999;
+        if( (long long)n > (long long)10000000) {
+            cnt += (long long)((long long)2*(long long)((long long)n-(long long)99999));
+            n = (long long)100000;
         }
-        if( (long long)n >= (long long)100000) {
+        if( (long long)n > (long long)100000) {
             cnt += (long long)((long long)n-(long long)999);
             n = (long long)999;
         }
-        if( (long long)n >= (long long)1000) {
+        if( (long long)n > (long long)1000) {
             cnt += (long long)((long long)n-(long long)999);
         }
         return cnt;
