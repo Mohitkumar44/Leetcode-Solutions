c53078e0c1b41294025664508b45f3aff3d400a9
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-09-09 18:09:38 +0530

    Update Count Commas in Range II solution

--- 4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     long long countCommas(long long n) {
         if( n < (long long)1000) return 0;
-        int cnt = 0;
-        while(n > (long long)1000) {
-            cnt++;
-            n/=100;
-        }
-        return cnt;
+        else if( n < (long long)100000) return n-999;
+        else if( n < (long long)10000000) return n-99999;
+        else if( n < (long long)1000000000) return n-9999999;
+        else if( n < (long long)100000000000) return n-999999999;
+        else if( n < (long long)10000000000000) return n-99999999999;
+        return -1;
     }
 };
